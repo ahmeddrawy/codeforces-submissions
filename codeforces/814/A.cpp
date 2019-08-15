1038837ffeab801e7adf02d033de46ac96451844
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2019-08-15 07:38:00 +0200

    Add solution for problem `A - An abandoned sentiment from past` Link: https://codeforces.com/contest/814/problem/A Tags: constructive algorithms, greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/814/submission/58846542

---
+++ codeforces/814/A.cpp
@@ -0,0 +1,84 @@
+//
+// Created by ahmed_drawy on 15/08/19.
+// virtual contest sheet
+#include <bits/stdc++.h>
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedMacroInspection"
+using namespace std;
+#define lp(i,start , end)       for(int i = start ; i<end ; ++i)
+#define lllp(i,start , end)       for(ll i = start ; i<end ; ++i)
+#define inN(arr ,  n) for(int i = 0 ; i< n ; ++i)  cin>>arr[i];
+#define Rlp(i,start , end)      for(int i = start ; i>end ; --i)
+#define all(v)                   ((v).begin(),(v).end())
+#define sz(v)                  (int)((v).size())
+#define clr(v,d)                memset(v , d , sizeof(v))
+
+#define inf 0x3f3f3f3f
+typedef long long               ll;
+//typedef first                    ft;
+//typedef second                   sd;
+typedef vector<int>             vi;
+typedef vector  <ll>            vll;
+typedef vector<vector<int> >    adj;
+typedef pair<int ,int>          pii;
+const double EPS =1e-7;
+const int OO = 1e6;
+bool sortpair( const pair<int , int> & x, const pair<int , int> & y){
+    return  x.first != y.first ? x.first < y.first : x.second > y.second ;
+}
+int dcomp(double x , double y){
+    return fabs(x-y) <= EPS? 0: x>y?1:-1;
+}
+void smile() {
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("/home/tw3/Desktop/training/in.txt", "r", stdin);
+//    freopen("/home/tw3/Desktop/training/out.txt" , "w" , stdout);
+#endif // ONLINE_JUDGE
+}
+const long long modul = 1000000007;
+
+ll mod(ll x) {
+    return (x%modul + modul)%modul;
+}
+int arr[101], ks[101];
+int main() {
+    smile();
+    int n , k ; cin >> n >> k ;
+    inN(arr, n );
+    inN(ks , k );
+    sort(ks , ks +k );
+    lp(i,1,n ){
+        if(arr[i] < arr[i-1] && arr[i] != 0 ){
+            cerr<<"1";
+            cout<<"Yes";
+            return 0 ;
+        }
+        else if(arr[i] == arr[i-1] && arr[i] == 0 ){
+            cerr<<"2";
+            cout  <<"Yes";
+            return 0 ;
+        }
+    }
+    lp(i,0,n ){
+        if(arr[i] == 0 ){
+           if(!i){
+               if(ks[k-1] > arr[i+1]){ // check largest
+                   cerr<<"3";
+                   cout  <<"Yes";
+                   return 0 ;
+               }
+
+           }
+           else {
+               if(ks[0] < arr[i-1] || (ks[k-1] > arr[i+1] && i+1 < n)){
+                   cerr<<"4";
+                   cout  <<"Yes";
+                   return 0 ;
+               }
+           }
+        }
+    }
+    cout <<"NO";
+
+}
