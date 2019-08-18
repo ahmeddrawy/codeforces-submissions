792fff00ef7e4855b5139c95c72bcf4491dc875b
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2019-08-18 15:05:00 +0200

    Add solution for problem `C - Trains` Link: https://codeforces.com/contest/88/problem/C Tags: implementation, math, number theory, *1500 Ref: https://codeforces.com/contest/88/submission/58999576

---
+++ codeforces/88/C.cpp
@@ -0,0 +1,59 @@
+//
+// Created by ahmed_drawy on 17/08/19.
+// virtual contest Educational Codeforces Round 70
+#include <bits/stdc++.h>
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedMacroInspection"
+using namespace std;
+#define lp(i,start , end)       for(int i = start ; i<end ; ++i)
+#define lllp(i,start , end)       for(ll i = start ; i<end ; ++i)
+#define inN(arr ,  n) for(int i = 0 ; i< n ; ++i)  cin>>arr[i];
+#define Rlp(i,start , end)      for(int i = start ; i>end ; --i)
+#define all(v)                   ((v).begin(),(v).end())
+#define sz(v)                  (int)((v).size())
+#define clr(v,d)                memset(v , d , sizeof(v))
+
+#define inf 0x3f3f3f3f
+typedef long long               ll;
+//typedef first                    ft;
+//typedef second                   sd;
+typedef vector<int>             vi;
+typedef vector  <ll>            vll;
+typedef vector<vector<int> >    adj;
+typedef pair<int ,int>          pii;
+const double EPS =1e-7;
+const int OO = 1e6;
+bool sortpair( const pair<int , int> & x, const pair<int , int> & y){
+    return  x.first != y.first ? x.first < y.first : x.second > y.second ;
+}
+int dcomp(double x , double y){
+    return fabs(x-y) <= EPS? 0: x>y?1:-1;
+}
+void smile() {
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("/home/tw3/Desktop/training/in.txt", "r", stdin);
+//    freopen("/home/tw3/Desktop/training/out.txt" , "w" , stdout);
+#endif // ONLINE_JUDGE
+}
+const long long modul = 1000000007;
+ll mod(ll x) {
+    return (x%modul + modul)%modul;
+}
+
+/// the min one will be repeated more till the LCM
+/// corner case when diff between them is 1 then the lcm will cover it
+int main() {
+    smile();
+    ll a , b; cin >> a>> b;
+    ll lcm = (a*b)/ __gcd(a,b);
+    a = lcm / a;
+    b = lcm / b;
+    if(abs(a-b) ==1){
+        cout<<"Equal";
+        return 0;
+    }
+    if(a>b)cout<<"Dasha";
+    else cout<<"Masha";
+
+}
