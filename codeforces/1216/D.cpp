e77f26bdaee293cb222c44ef826b7cc5a330cf69
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2019-09-21 10:19:00 +0200

    Add solution for problem `D - Swords` Link: https://codeforces.com/contest/1216/problem/D Tags: math, *1300 Ref: https://codeforces.com/contest/1216/submission/60989102

---
+++ codeforces/1216/D.cpp
@@ -0,0 +1,106 @@
+///Codeforces Round #587 (Div. 3)
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define inN(arr ,  n) for(int i = 0 ; i< n ; ++i)  cin>>arr[i];
+#define lp(i,start , end)       for(int i = start ; i<end ; ++i)
+#define sz(v)                  (int)((v).size())
+#define clr(v,d)                memset(v , d , sizeof(v))
+#define pii pair<int, int>
+#define vii vector<ll>
+#define   endl '\n'
+void smile() {
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("/home/tw3/Desktop/Desktop/training/in.txt", "r", stdin);
+//    freopen("/home/tw3/Desktop/Desktop/training/out.txt" , "w" , stdout);
+#endif
+}
+
+int gcd(int a, int b, int &x, int &y) {
+    if (a == 0) {
+        x = 0; y = 1;
+        return b;
+    }
+    int x1, y1;
+    int d = gcd(b%a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return d;
+}
+
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+    g = gcd(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
+void shift_solution(int & x, int & y, int a, int b, int cnt) {
+    x += cnt * b;
+    y -= cnt * a;
+}
+
+int find_all_solutions(int a, int b, int c, int minx, int maxx, int miny, int maxy) {
+    int x, y, g;
+    if (!find_any_solution(a, b, c, x, y, g))
+        return 0;
+    a /= g;
+    b /= g;
+
+    int sign_a = a > 0 ? +1 : -1;
+    int sign_b = b > 0 ? +1 : -1;
+
+    shift_solution(x, y, a, b, (minx - x) / b);
+    if (x < minx)
+        shift_solution(x, y, a, b, sign_b);
+    if (x > maxx)
+        return 0;
+    int lx1 = x;
+
+    shift_solution(x, y, a, b, (maxx - x) / b);
+    if (x > maxx)
+        shift_solution(x, y, a, b, -sign_b);
+    int rx1 = x;
+
+    shift_solution(x, y, a, b, -(miny - y) / a);
+    if (y < miny)
+        shift_solution(x, y, a, b, -sign_a);
+    if (y > maxy)
+        return 0;
+    int lx2 = x;
+
+    shift_solution(x, y, a, b, -(maxy - y) / a);
+    if (y > maxy)
+        shift_solution(x, y, a, b, sign_a);
+    int rx2 = x;
+
+    if (lx2 > rx2)
+        swap(lx2, rx2);
+    int lx = max(lx1, lx2);
+    int rx = min(rx1, rx2);
+
+    if (lx > rx)
+        return 0;
+    return (rx - lx) / abs(b) + 1;
+}
+
+int main(){
+    smile();
+    int n ; cin >> n ;
+    vector<ll > vec(n);
+    inN(vec , n);
+    sort(vec.rbegin() , vec.rend());
+    ll sum = 0 ;
+    ll g = 0 ;
+    for (int j = 1; j <n ; ++j) {
+        sum+=vec[0]- vec[j];
+        g= __gcd( g ,vec[0]- vec[j] );
+    }
+    cout << sum/g<<" " << g;
+}
