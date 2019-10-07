8fcf3e1b42da33c60f6ccbe7c2df55ff60c7ff11
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2019-10-07 17:28:00 +0200

    Add solution for problem `A - CME` Link: https://codeforces.com/contest/1241/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1241/submission/62066765

---
+++ codeforces/1241/A.cpp
@@ -0,0 +1,52 @@
+/// 147 -uva - by ahmed_drawy 28/09/19
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define inN(arr ,  n) for(int i = 0 ; i< n ; ++i)  cin>>arr[i];
+#define lp(i,start , end)       for(int i = start ; i<end ; ++i)
+#define sz(v)                  (int)((v).size())
+#define clr(v,d)                memset(v , d , sizeof(v));
+#define pii pair<int, int>
+#define vii vector<ll>
+#define   endl '\n'
+#define debug(x) cerr << x<<endl;
+void smile() {
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("/home/tw3/Desktop/Desktop/training/in.txt", "r", stdin);
+//    freopen("/home/tw3/Desktop/Desktop/training/out.txt" , "w" , stdout);
+#endif
+}
+int randomF() {
+    return  (rand() * RAND_MAX + rand()) % 1000000;
+
+}
+
+int MOD ;
+int mod(ll x){
+    return (x%MOD + MOD)%MOD;
+}
+long long add(long long x, long long y)
+{
+    x += y;
+    while(x >= MOD)
+        x -= MOD;
+    while(x < 0)
+        x += MOD;
+    return x;
+}
+ll multipy(ll x , ll y ){
+    return ((x%MOD) *(y%MOD))%MOD;
+}
+
+int main() {
+    smile();
+    int n ; cin >> n;
+     while(n-- ){
+         int x ; cin >> x;
+         if(x == 2)cout << x;
+         else cout << (x&1);
+        cout << endl;
+
+     }
+}
