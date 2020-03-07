8b99694928db3f0fe2a4411ba269b10a7ed4582d
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-03-07 10:49:00 +0200

    Add solution for problem `B - Art Union` Link: https://codeforces.com/contest/416/problem/B Tags: brute force, dp, implementation, *1300 Ref: https://codeforces.com/contest/416/submission/72625500

---
+++ codeforces/416/B.cpp
@@ -0,0 +1,107 @@
+/// by ahmed_drawy 20/12/19
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+using namespace std;
+#define ll long long
+#define inN(arr ,  n) for(int i = 0 ; i< n ; ++i)  cin>>arr[i];
+#define lp(i,start , end)       for(int i = start ; i<end ; ++i)
+#define sz(v)                  (int)((v).size())
+#define clr(v,d)                memset(v , d , sizeof(v));
+#define pii pair<int, int>
+#define vii vector<ll>
+#define vi vector<int>
+#define   endl '\n'
+#define debug(x) cerr << x<<endl;
+const double EPS = (1e-7);
+void smile() {
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("/home/tw3/Desktop/Desktop/training/in.txt", "r", stdin);
+  //  freopen("/home/tw3/Desktop/Desktop/training/out.txt" , "w" , stdout);
+#endif
+}
+int dcmp(double x, double y) {	return fabs(x-y) <= EPS ? 0 : x < y ? -1 : 1;	}
+int randomF() {
+    return  (rand() * RAND_MAX + rand()) % 1000000;
+
+}
+
+ll MOD =(ll) 1e9 +7 ;
+int mod(ll x){
+    return (x%MOD + MOD)%MOD;
+}
+long long add(long long x, long long y)
+{
+    x += y;
+    while(x >= MOD)
+        x -= MOD;
+    while(x < 0)
+        x += MOD;
+    return x;
+}
+ll multipy(ll x , ll y ){
+    return ((x%MOD) *(y%MOD))%MOD;
+}
+long long power(long long base, int exp)
+{
+    if (!exp)
+        return 1LL;
+    ll sq = power(base, exp/2);
+    sq = (sq * sq)%MOD;
+    if (exp&1)
+        sq = (sq * base)%MOD;
+    return sq;
+}
+bool compareT(pair<int , int > & a , pair<int  ,int > &b){
+    if(a.first != b.first)
+        return a.first <b.first ;
+    return a.second< b.second ;
+}
+double dist(int x , int y , int a , int b){
+    return sqrt( (x-a)*(x-a) + (y-b)*(y-b));
+
+}
+bool isPalindrome (string s , int m ){
+    for (int i = 0; i <m/2 ; ++i) {
+        if(s[i] != s[m-i-1])
+            return false ;
+    }
+    return true;
+
+}
+int dp[50005][11] ;
+int arr[50005][11] ;
+
+int main(){
+    smile();
+    int n , m ; cin >> m >> n ;
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+    dp[0][0] = arr[0][0];
+    for (int i = 1; i < m; ++i) {
+        dp[i][0] = dp[i-1][0]+arr[i][0];
+    }
+
+    for (int i = 0; i < m; ++i) {
+        for (int k = 1; k <n ; ++k) {
+            dp[i][k]=dp[i][k-1]+arr[i][k];
+            if(i){
+                dp[i][k] = max(dp[i-1][k] +arr[i][k] , dp[i][k]);
+            }
+        }
+    }
+
+
+    for(int i = 0 ; i < m; ++i){
+        cout << dp[i][n-1] << " ";
+    }
+
+
+}
