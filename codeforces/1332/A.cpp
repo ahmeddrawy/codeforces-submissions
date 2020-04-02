b5cc47ff0c1253fd4bd5c43af1cfe4bddc6d6f06
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-04-02 19:10:00 +0200

    Add solution for problem `A - Exercising Walk` Link: https://codeforces.com/contest/1332/problem/A Tags: greedy, implementation, math, *1100 Ref: https://codeforces.com/contest/1332/submission/75269531

---
+++ codeforces/1332/A.cpp
@@ -0,0 +1,93 @@
+/// by ahmed_drawy 15/03/20
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+using namespace std;
+#define ll long long
+#define inN(arr ,  n) for(int i = 0 ; i< n ; ++i)  cin>>arr[i];
+#define lp(i,start , end)       for(int i = start ; i<end ; ++i)
+#define sz(v)                  (int)((v).size())
+#define clr(v,d)                memset(v , d , sizeof(v));
+#define pii pair<int, int>
+#define vii vector<ll>
+#define vi vector<int>
+#define   endl '\n'
+#define debug(x) cerr << x<<endl;
+const double EPS = (1e-7);
+void smile() {
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("/home/tw3/Desktop/Desktop/training/in.txt", "r", stdin);
+//    freopen("/home/tw3/Desktop/Desktop/training/out.txt" , "w" , stdout);
+#endif
+}
+int dcmp(double x, double y) {	return fabs(x-y) <= EPS ? 0 : x < y ? -1 : 1;	}
+int randomF() {
+    return  (rand() * RAND_MAX + rand()) % 1000000;
+
+}
+
+ll MOD =(ll) 1e9 +7 ;
+int mod(ll x){
+    return (x%MOD + MOD)%MOD;
+}
+long long add(long long x, long long y){
+    x += y;
+    while(x >= MOD)
+        x -= MOD;
+    while(x < 0)
+        x += MOD;
+    return x;
+}
+ll multipy(ll x , ll y ){
+    return ((x%MOD) *(y%MOD))%MOD;
+}
+long long power(long long base, int exp){
+    if (!exp)
+        return 1LL;
+    ll sq = power(base, exp/2);
+    sq = (sq * sq)%MOD;
+    if (exp&1)
+        sq = (sq * base)%MOD;
+    return sq;
+}
+bool compareT(pair<int , int > & a , pair<int  ,int > &b){
+    if(a.first != b.first)
+        return a.first <b.first ;
+    return a.second< b.second ;
+}
+double dist(int x , int y , int a , int b) {
+    return sqrt((x - a) * (x - a) + (y - b) * (y - b));
+
+}
+void solve(){
+    int a , b , c , d  ; cin >> a>>b  >> c >> d;
+    int x , y , x1 ,y1 , x2 , y2 ;
+    cin >> x >> y >> x1 >> y1 >> x2 >> y2;
+    int dx = abs(a-b);
+    int dy = abs(c-d);
+    int dyy = abs(y1 - y2);
+    int dxx = abs(x1 - x2);
+    if(dyy == 0 && (c || d)){
+        cout << "NO";
+    }
+    else if(dxx == 0 &&(a || b)){
+        cout << "NO";
+    }
+    else if(x - (a-b) > x2 || x-(a-b)  < x1 ||y-(c-d)  < y1||y-(c-d)  > y2){
+        cout << "NO";
+    }
+    else cout << "YES";
+    cout << endl;
+
+}
+
+int main() {
+    smile();
+    int t ; cin >> t;
+    while(t--)
+        solve();
+}
